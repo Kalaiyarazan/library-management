f60cc76d3c47fc0cb43041ef4db59cd8b5817365
kalaiyarazan <kalaiyarazan.v@gmail.com>
2019-10-09 10:54:41 +0530

    Added isAvailable, isInCart (May Require Change)

--- src/components/Book.js
+++ src/components/Book.js
@@ -3,7 +3,9 @@ import {Button, Modal, ModalHeader, ModalBody, ModalFooter} from 'reactstrap'
 
   class Book extends Component {
       state={
-        modal: false
+        modal: false,
+        isAvailable:true,
+        isInCart:false
       }
 
       toggle=()=> {
@@ -11,18 +13,25 @@ import {Button, Modal, ModalHeader, ModalBody, ModalFooter} from 'reactstrap'
           modal: !prevState.modal
         }));
     }
+
+    onAddCart=()=>{
+        this.setState(prevState => ({
+            isInCart: !prevState.isInCart
+          }));
+    }
     
     render() {
         const {id,title, author, genre, yearofpublication}=this.props.book
-        return (
-            <div>
+        const {modal, isAvailable, isInCart}= this.state
+        return (               
+            <div >
                 <img src="http://tiny.cc/kc34dz" alt="book_thumbnail" width="150" />
                 <h5>{title}</h5>
                 <h6>Genre: {genre}</h6>
                 <h6>Author: {author}</h6>
                 <h6>Year of Publication: {yearofpublication}</h6>
                 <Button color="danger" onClick={this.toggle}>Get Now</Button>
-                <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+                <Modal isOpen={modal} toggle={this.toggle} className={this.props.className}>
                 <ModalHeader toggle={this.toggle}>
                     <h3>{title}</h3>
                     <p style={{fontSize:"15px", color:"blue"}}>by {author}</p>
@@ -35,12 +44,26 @@ import {Button, Modal, ModalHeader, ModalBody, ModalFooter} from 'reactstrap'
                     <p><em>Genre: {genre}</em></p>
                     <p><em>Published on: {yearofpublication}</em></p>
                     <strong>Short Introduction: </strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-
                     </div>
                 </ModalBody>
                 <ModalFooter>
-                    <Button color="primary" onClick={this.toggle}>Continue</Button>{' '}
-                    <Button color="warning" onClick={this.toggle}>Add to Cart</Button>
+                    {isAvailable?
+                    <>
+                        <Button color="success">Available</Button>
+                        <Button color="primary" onClick={this.toggle}>Continue</Button>
+                            {isInCart?
+                                <Button color="warning" onClick={this.onAddCart}>Add to Cart</Button>
+                                :
+                                <Button color="warning" onClick={this.onAddCart}>Added to Cart</Button>
+                            }
+                    </>
+                        :
+                    <>
+                        <Button color="danger">Not Available</Button>
+                        <Button color="primary" onClick={this.toggle}>Continue</Button>
+                        <Button color="warning" className="disabled">Add to Cart</Button>
+                    </>
+                    }
                 </ModalFooter>
                 </Modal>
                 <br/><br/>
