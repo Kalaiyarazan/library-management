b1ec9859e8de89ac6b56a60939b744f3bcbd96b5
kalaiyarazan <kalaiyarazan.v@gmail.com>
2019-10-18 11:01:19 +0530

    Updates

---
+++ precache-manifest.91273e4afe22a992af193b5bd91c0be7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e1043d8c8ce22081106186960d989f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83d34cc7c70f0909489f",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "3e4b25ec3cd3ded97286",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "83d34cc7c70f0909489f",
+    "url": "/static/js/2.5447c632.chunk.js"
+  },
+  {
+    "revision": "3e4b25ec3cd3ded97286",
+    "url": "/static/js/main.f84a407c.chunk.js"
+  },
+  {
+    "revision": "6fc4ef4f215b04457160",
+    "url": "/static/js/runtime-main.5dd0c537.js"
+  }
+]);
